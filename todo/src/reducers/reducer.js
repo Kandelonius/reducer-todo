678406b03ca74388714e9b0a782794096c5ad040
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-11 13:08:08 -0700

    have the project set up and completed step 1.

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+import React, { useState, useReducer } from "react";
+
+
+export default reducer = (state, action) => {
+    return { item: "" };
+}
+export default initialState =
+{
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
